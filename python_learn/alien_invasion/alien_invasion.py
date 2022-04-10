991229de8782bf80d30fdc6e2ad55292a13399c0
fhe <fhe142857@163.com>
2022-04-10 13:38:50 +0800

    Initial commit

---
+++ python_learn/alien_invasion/alien_invasion.py
@@ -0,0 +1,35 @@
+# @Time : 2022/4/9 15:32
+# @Author :贺
+# @File :alien_invasion.py
+# @Software:PyCharm
+import sys
+
+import pygame
+
+
+class Alien_invasion:
+    """管理游戏资源和行为的类"""
+
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            # 监视键盘和鼠标事件
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # 创建游戏实例并运行游戏
+    ai = Alien_invasion()
+    ai.run_game()
