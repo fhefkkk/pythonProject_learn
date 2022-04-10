532649b4a83bef56045d797b26f04508b462e12f
fhe <fhe142857@163.com>
2022-04-10 16:24:49 +0800

    draw ship on screen

--- python_learn/alien_invasion/alien_invasion.py
+++ python_learn/alien_invasion/alien_invasion.py
@@ -7,6 +7,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -19,6 +20,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         # 设置背景色
         self.screen.fill(self.settings.bg_color)
@@ -33,6 +36,7 @@ class AlienInvasion:
 
             # 每次循环时都绘制屏幕
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # 让最近绘制的屏幕可见
             pygame.display.flip()
